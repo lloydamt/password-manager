dd45a973f971d6d2882654f9b6d44c021ea61f5f
Adawari <adawarithomas@gmail.com>
2022-01-27 13:16:54 +0000

    Complete file save functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
 
 window = Tk()
 window.title("Password Manager")
@@ -11,6 +12,27 @@ canvas = Canvas(width=200, height=200)
 canvas_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=canvas_img)
 canvas.grid(column=1, row=0)
+
+
+"---------------------------------------Save to File---------------------------------------------------------------"
+def add_entry():
+
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showinfo(title="error", message="Please fill all fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered\nEmail: {username}\n"
+                                                              f"Password: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("password_entries.txt", "a") as file:
+                file.write(f"{website} | {username} | {password}")
+                file.write("\n")
+            website_entry.delete(0, "end")
+            password_entry.delete(0, "end")
+            website_entry.focus()
 
 
 "--------------------------------------------User Interface-------------------------------------------------------"
