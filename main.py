c08fcc9c86800fdb832fef3ee5b3939a34d3906e
Adawari <adawarithomas@gmail.com>
2022-02-01 22:42:11 +0000

    add search functionality

--- main.py
+++ main.py
@@ -37,6 +37,39 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+# -------------------------------------- Search ------------------------------------------------------------------
+
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="error", message="No data file found")
+    except json.decoder.JSONDecodeError:
+        messagebox.showinfo(title="error", message="Empty data file")
+    else:
+        if len(website) == 0:
+            messagebox.showinfo(title="error", message="Please enter a website")
+        elif website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="error", message="website does not exist in data")
+
+        # isMatch = False
+        # for (site, details) in data.items():
+        #     if len(website) == 0:
+        #         messagebox.showinfo(title="error", message="Please input a website")
+        #         break
+        #     if site == website:
+        #         messagebox.showinfo(title=f"{site}", message=f'Email: {details["email"]} \n'
+        #                                                         f'Password: {details["password"]}')
+        #         isMatch = True
+        # if not isMatch and len(website) > 0:
+        #     messagebox.showinfo(title="error", message="Website does not exist in database")
+
 
 "---------------------------------------Save to File---------------------------------------------------------------"
 def add_entry():
@@ -57,24 +90,36 @@ def add_entry():
         # is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered\nEmail: {username}\n"
         #                                                       f"Password: {password} \nIs it ok to save?")
         # if is_ok:
-        with open("data.json", "r") as read_file:
-            data = json.load(read_file)
-            data.update(new_data)
-        with open("data.json", "w") as file:
-            json.dump(data, file, indent=4)
+        try:
+            with open("data.json", "r") as read_file:
+                data = json.load(read_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        except json.decoder.JSONDecodeError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            with open("data.json", "w") as file:
+                data.update(new_data)
+                json.dump(data, file, indent=4)
 
-            website_entry.delete(0, "end")
-            password_entry.delete(0, "end")
-            website_entry.focus()
+        finally:
+                website_entry.delete(0, "end")
+                password_entry.delete(0, "end")
+                website_entry.focus()
 
 
 "--------------------------------------------User Interface-------------------------------------------------------"
 # Buttons
-generate_button = Button(text="Generate Password", command=generate_password)
-generate_button.grid(column=2, row=3)
+generate_button = Button(text="Generate Password", command=generate_password, width=15)
+generate_button.grid(column=2, row=3, sticky=E)
 
-add_button = Button(text="Add", width=37, command=add_entry)
-add_button.grid(column=1, row=4, columnspan=2, sticky=E)
+add_button = Button(text="Add", width=45, command=add_entry)
+add_button.grid(column=1, row=4, columnspan=2, sticky=W)
+
+search_button = Button(text="Search", command=search, width=15)
+search_button.grid(column=2, row=1, sticky=E)
 
 # labels
 website_label = Label(text="Website:", font=("Roboto", 10))
@@ -88,16 +133,16 @@ password_label.grid(column=0, row=3)
 
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
 website_entry.focus()
 
-username_entry = Entry(width=35)
-username_entry.grid(column=1, row=2, columnspan=2)
+username_entry = Entry(width=53)
+username_entry.grid(column=1, row=2, columnspan=2, sticky=W)
 username_entry.insert(0, "lloydamt@gmail.com")
 
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry = Entry(width=33)
+password_entry.grid(column=1, row=3, sticky=W)
 
 
 
